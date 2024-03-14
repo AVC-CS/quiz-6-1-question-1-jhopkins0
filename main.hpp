560aa0c0ac17eccb0b174840a0d4a74f10164970
Jordan Hopkins <jhopkins4@students.avc.edu>
2024-03-13 20:29:46 -0700

    a

--- main.hpp
+++ main.hpp
@@ -1,4 +1,42 @@
+int getTwoValues(int, int);
+bool Prime(int);
+int nextPrime(int);
+int prevPrime(int);
 
+
+int getTwoValues(int &begin, int &end){
+    do{
+        cin >> begin >> end;
+    }while(begin >= end);
+}
+
+bool Prime(int num){
+    if(num <= 1){
+        return false;
+    }
+    for(int i = 2; i * i <= num; i++){
+        if(num % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int nextPrime(int begin){
+    for(int i = begin + 1; i++){
+        if(Prime(i)){
+            return i;
+        }
+    }
+}
+
+int prevPrime(int end){
+    for(int i = end - 1; i > 1;){
+        if(Prime(i)){
+            return i;
+        }
+    }
+}
 //********************
 // Declare Function Prototypes
 //********************
