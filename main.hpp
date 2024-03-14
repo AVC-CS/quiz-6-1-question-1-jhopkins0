81090f0d9804346aa1613ee24e18a3d6728757e1
Jordan Hopkins <jhopkins4@students.avc.edu>
2024-03-13 20:58:39 -0700

    test

--- main.hpp
+++ main.hpp
@@ -8,35 +8,35 @@ int getNextPrime(int);
 int getPrevPrime(int);
 
 
-int getTwoValues(int &begin, int &end){
-    do{
+int getTwoValues(int &begin, int &end) {
+    do {
         cin >> begin >> end;
-    }while(begin >= end);
+    } while (begin >= end);
 }
 
-bool isPrime(int num){
-    if(num <= 1){
+bool isPrime(int n) {
+    if(n <= 1) {
         return false;
     }
-    for(int i = 2; i * i <= num; i++){
-        if(num % i == 0){
+    for(int i = 2; i*i <= n; i++) {
+        if(n % i == 0) {
             return false;
         }
     }
     return true;
 }
 
-int getNextPrime(int begin){
-    for(int i = begin + 1; ; i++){
-        if(isPrime(i)){
+int getNextPrime(int begin) {
+    for(int i = begin + 1; ; i++) {
+        if(isPrime(i)) {
             return i;
         }
     }
 }
 
-int getPrevPrime(int end){
-    for(int i = end - 1; i > 1;){
-        if(isPrime(i)){
+int getPrevPrime(int end) {
+    for(int i = end - 1; i > 1; i--) {
+        if(isPrime(i)) {
             return i;
         }
     }
